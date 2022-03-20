431788804970e7bb14436617272ee2d9f616a904
ardi renaldi <renaldimedia@gmail.com>
2022-03-20 17:11:49 +0700

    add admin filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,11 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'adminfilter']);
+
+$routes->group('admin', function ($routes) {
+    $routes->get('admins', 'App\Admins::index');
+});
 
 /*
  * --------------------------------------------------------------------
